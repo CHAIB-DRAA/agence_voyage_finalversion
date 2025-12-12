36c2637fc66fb3048c4a3535b0ccf16cf29e20ee
chaib-draa <chaibdraa.laid@gmail.com>
2025-12-12 14:44:12 +0100

    test

--- Backend_taxi/routes/settingRoutes.js
+++ Backend_taxi/routes/settingRoutes.js
@@ -1,24 +1,21 @@
 const express = require('express');
 const router = express.Router();
+// IMPORTANT : Respecter la majuscule du fichier (SettingController) pour Linux/Render
 const settingController = require('../controllers/settingController');
 
 // --- ROUTES /settings ---
 
 // Récupérer tous les paramètres (groupés par catégorie)
-// GET /settings
 router.get('/', settingController.getSettings);
-router.post('/', settingController.addSetting);
 
 // Créer un nouveau paramètre
-// POST /settings
-router.post('/', settingController.createSetting);
+// CORRECTION : La fonction exportée dans le contrôleur est 'addSetting'
+router.post('/', settingController.addSetting);
 
 // Modifier un paramètre existant (ex: changer son prix)
-// PUT /settings/:id
 router.put('/:id', settingController.updateSetting);
 
 // Supprimer un paramètre
-// DELETE /settings/:id
 router.delete('/:id', settingController.deleteSetting);
 
 module.exports = router;
